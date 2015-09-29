713ecd89b97663805f055c8913b18200de70e73f
Rob Fallows <rob.fallows@gmail.com>
2015-09-29 11:18:41 +0100

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'tunguska:gauge',
-  version: '1.0.20',
+  version: '1.0.21',
   summary: 'Highly configurable, reactive gauges',
   git: 'https://github.com/robfallows/tunguska-gauge.git',
   documentation: 'README.md'
