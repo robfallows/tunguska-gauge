409d3e4fd1b3fb1edef34fe0ad318cc209aafb95
Rob Fallows <rob.fallows@gmail.com>
2015-09-29 11:11:50 +0100

    package update for Meteor 1.2

--- package.js
+++ package.js
@@ -1,21 +1,25 @@
 Package.describe({
   name: 'tunguska:gauge',
-  version: '1.0.19',
+  version: '1.0.20',
   summary: 'Highly configurable, reactive gauges',
   git: 'https://github.com/robfallows/tunguska-gauge.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.3.1');
+  if (api.addAssets) {
+    api.versionsFrom('1.2');
+  } else {
+    api.versionsFrom('1.0.3.1');
+  }
   api.use('templating', 'client');
-  api.addFiles('tunguska:gauge.js','client');
+  api.addFiles('gauge.js','client');
   api.export('TunguskaGauge','client');
 });
 
 Package.onTest(function(api) {
   api.use('templating', 'client');
   api.use('practicalmeteor:munit','client');
-  api.use('tunguska:gauge','client');
+  api.use('gauge','client');
   api.addFiles('tests/client-tests.js','client');
 });
